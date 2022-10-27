284bbad8a3d3665ccc242905a7cd563f45bdfbdc
Maximix206p2 <114424597+Maximix206p2@users.noreply.github.com>
2022-10-27 20:11:27 +0300

    Update JavaCR2.java

--- JavaCR2.java
+++ JavaCR2.java
@@ -25,5 +25,38 @@ public class JavaCR2 {
             }
             System.out.println();
         }
+        //2
+        int cops = 99;
+        System.out.println(cops);
+        while (cops - 50 >= 0) {
+            System.out.printf("%d - 50 = %d", cops, cops - 50);
+            cops -= 50;
+            System.out.println();
+        }
+        while (cops - 20 >= 0) {
+            System.out.printf("%d - 20 = %d", cops, cops - 20);
+            cops -= 20;
+            System.out.println();
+        }
+        while (cops - 10 >= 0) {
+            System.out.printf("%d - 10 = %d", cops, cops - 10);
+            cops -= 10;
+            System.out.println();
+        }
+        while (cops - 5 >= 0) {
+            System.out.printf("%d - 5 = %d", cops, cops - 5);
+            cops -= 5;
+            System.out.println();
+        }
+        while (cops - 2 >= 0) {
+            System.out.printf("%d - 2 = %d", cops, cops - 2);
+            cops -= 2;
+            System.out.println();
+        }
+        while (cops - 1 >= 0) {
+            System.out.printf("%d - 1 = %d", cops, cops - 1);
+            cops -= 1;
+            System.out.println();
+        }
     }
 }
