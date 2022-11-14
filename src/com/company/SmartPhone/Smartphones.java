4940eb1035baf0cd91a49dc1bf2e96689de60629
Maximix206p2 <114424597+Maximix206p2@users.noreply.github.com>
2022-11-14 21:29:44 +0300

    Delete src/com/company/SmartPhone directory

--- src/com/company/SmartPhone/Smartphones.java
+++
@@ -1,9 +0,0 @@
-package com.company.SmartPhone;
-
-import java.util.LinkedList;
-
-//Каталог смартфонов
-public class Smartphones {
-    private LinkedList<Smartphone> smartphones = new LinkedList<>();
-
-}
